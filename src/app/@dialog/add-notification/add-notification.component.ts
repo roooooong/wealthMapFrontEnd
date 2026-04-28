20c6bdc767a883648b0122d42f6fb1267a027430
roooooong <mandy0220405@gmail.com>
2026-04-28 11:24:26 +0800

    公告設定必填判斷、header鈴鐺RWD
    公告設定必填判斷、header鈴鐺RWD

--- src/app/@dialog/add-notification/add-notification.component.ts
+++ src/app/@dialog/add-notification/add-notification.component.ts
@@ -21,6 +21,35 @@ export class AddNotificationComponent {
   notificationList!: Data;
   editNotificationList!: Data;
 
+  tagErrorMsg = '';
+  titleErrorMsg = '';
+  scheduledDateErrorMsg = '';
+
+  validate(field: 'tag' | 'title' | 'scheduledDate'): void {
+
+    if (field === 'tag') {
+      if (!this.tag) {
+        this.tagErrorMsg = '*必填';
+      } else {
+        this.tagErrorMsg = ''; // 格式正確就清空訊息
+      }
+    }
+    if (field === 'title') {
+      if (!this.title) {
+        this.titleErrorMsg = '*必填';
+      } else {
+        this.titleErrorMsg = '';
+      }
+    }
+    if (field === 'scheduledDate') {
+      if (!this.scheduledDate) {
+        this.scheduledDateErrorMsg = '*必填';
+      } else {
+        this.scheduledDateErrorMsg = '';
+      }
+    }
+  }
+
 
   constructor(
     private httpClientService: HttpClientService) {
@@ -35,34 +64,46 @@ export class AddNotificationComponent {
   gettoday!: string;
 
   save() {
-    this.notificationList.tag = this.tag
-    this.notificationList.title = this.title
-    this.notificationList.content = this.content
-    this.notificationList.scheduledDate = this.scheduledDate
-    this.httpClientService.postApi(`http://localhost:8080/api/notifications/save`, this.notificationList)
-      .subscribe((save: any) => {
-        if (save.code == 200) {
-          console.log('通知儲存成功');
-          this.dialogRef.close();
-        }
-        window.location.reload();
-      })
+    this.notificationList.tag = this.tag;
+    this.notificationList.title = this.title;
+    this.notificationList.content = this.content;
+    this.notificationList.scheduledDate = this.scheduledDate;
+    this.validate('tag');
+    this.validate('title');
+    this.validate('scheduledDate');
+
+    if (!this.tagErrorMsg && !this.titleErrorMsg && !this.scheduledDateErrorMsg) {
+      this.httpClientService.postApi(`http://localhost:8080/api/notifications/save`, this.notificationList)
+        .subscribe((save: any) => {
+          if (save.code == 200) {
+            console.log('通知儲存成功');
+            this.dialogRef.close();
+          }
+          window.location.reload();
+        })
+    }
   }
 
   edit() {
     this.editNotificationList.id = this.id;
-    this.editNotificationList.tag = this.tag
-    this.editNotificationList.title = this.title
-    this.editNotificationList.content = this.content
-    this.editNotificationList.scheduledDate = this.scheduledDate
-    this.httpClientService.putApi(`http://localhost:8080/api/notifications/update`, this.editNotificationList)
-      .subscribe((save: any) => {
-        if (save.code == 200) {
-          console.log('更新成功');
-          this.dialogRef.close();
-        }
-        window.location.reload();
-      })
+    this.editNotificationList.tag = this.tag;
+    this.editNotificationList.title = this.title;
+    this.editNotificationList.content = this.content;
+    this.editNotificationList.scheduledDate = this.scheduledDate;
+    this.validate('tag');
+    this.validate('title');
+    this.validate('scheduledDate');
+
+    if (!this.tagErrorMsg && !this.titleErrorMsg && !this.scheduledDateErrorMsg) {
+      this.httpClientService.putApi(`http://localhost:8080/api/notifications/update`, this.editNotificationList)
+        .subscribe((save: any) => {
+          if (save.code == 200) {
+            console.log('更新成功');
+            this.dialogRef.close();
+          }
+          window.location.reload();
+        })
+    }
   }
 
   cancel() {
@@ -92,22 +133,22 @@ export class AddNotificationComponent {
       hasRead: false
     };
     this.tag = '';
-    this.title ='';
+    this.title = '';
     this.content = '';
-    this.scheduledDate ='';
+    this.scheduledDate = '';
 
-    if(this.data.choise==2){
-    //取得修改公告列表
-    this.httpClientService.getApi(`http://localhost:8080/api/notifications/list`)
-      .subscribe((editNotificationList: any) => {
-        console.log(editNotificationList.data[this.data.index]);
-        this.id = editNotificationList.data[this.data.index].id;
-        this.tag = editNotificationList.data[this.data.index].tag;
-        this.title = editNotificationList.data[this.data.index].title;
-        this.content = editNotificationList.data[this.data.index].content;
-        this.scheduledDate = editNotificationList.data[this.data.index].scheduledDate;
-      })
-      }
+    if (this.data.choise == 2) {
+      //取得修改公告列表
+      this.httpClientService.getApi(`http://localhost:8080/api/notifications/list`)
+        .subscribe((editNotificationList: any) => {
+          console.log(editNotificationList.data[this.data.index]);
+          this.id = editNotificationList.data[this.data.index].id;
+          this.tag = editNotificationList.data[this.data.index].tag;
+          this.title = editNotificationList.data[this.data.index].title;
+          this.content = editNotificationList.data[this.data.index].content;
+          this.scheduledDate = editNotificationList.data[this.data.index].scheduledDate;
+        })
+    }
 
 
     //取得今天日期
